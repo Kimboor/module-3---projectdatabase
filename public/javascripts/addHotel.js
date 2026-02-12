9797c883e28620f709cfdedf45745310c1acf037
Ditt Navn <kimasim1@gmail.com>
2026-02-12 21:25:45 +0100

    Created reservation and add rooms

---
+++ public/javascripts/addHotel.js
@@ -0,0 +1,26 @@
+async function addHotel(url) {
+    let name = prompt("Provide the new hotel's name:");
+    let hotelLocation = prompt("Provide the new hotel's location:");
+    await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            Name: name,
+            Location: hotelLocation,
+        }),
+        })
+
+        .then((response) => {
+            if (response.ok) {
+                const resData = "Created a new Hotel";
+                location.reload();
+                return Promise.resolve(resData);
+            }
+            return Promise.reject(response);
+        })
+        .catch((response) => {
+            alert(response.statusText);
+        });
+}
